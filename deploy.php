26149ef8c8905f64de3b2ece1e2478760b1fc41c
Yao Wei (魏銘廷) <mwei@lxde.org>
2012-04-23 15:46:24 +0800

    add script to add theme hash for cache manifest

--- deploy.php
+++ deploy.php
@@ -43,6 +43,13 @@ function recompile_and_sync()
 	fclose ($fp);
 	print ("\n");
 
+	print ("= Writing commit hashes to manifest =\n");
+    chdir (THEME_PATH);
+    $theme_hash = trim(system("git rev-parse HEAD"));
+    chdir ($cwd);
+	$fp = fopen (TMP_PATH.'site.appcache', "a");
+    fwrite ($fp, "\n# THEME $theme_hash\n");
+
 	print ("= Syncing Content =\n");
 	system ('rsync -a --delete ' . TMP_PATH . ' ' . WEBSITE_PATH);
 	print ("\n");
